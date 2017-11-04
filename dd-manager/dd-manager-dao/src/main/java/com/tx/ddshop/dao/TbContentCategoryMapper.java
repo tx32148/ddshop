7f1bfc19c8a05e0e0f5775415b99c82a1f3e0e8a
javaTX <tx32148@163.com>
2017-11-04 16:22:57 +0800

    测试

--- dd-manager/dd-manager-dao/src/main/java/com/tx/ddshop/dao/TbContentCategoryMapper.java
+++ dd-manager/dd-manager-dao/src/main/java/com/tx/ddshop/dao/TbContentCategoryMapper.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
 public interface TbContentCategoryMapper {
+
     int countByExample(TbContentCategoryExample example);
 
     int deleteByExample(TbContentCategoryExample example);
